191641c5ed8631608579e280282f6ee59c22aff0
Valliyappan Kannappan <105097552+ValliyappanK@users.noreply.github.com>
2022-05-08 05:55:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents\' New Healthy Diner')
 
 streamlit.header('\N{bowl with spoon} Breakfast Favorites')
-streamlit.text('\N{bowl with spoon} Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('🍓 Avocado Toast')
