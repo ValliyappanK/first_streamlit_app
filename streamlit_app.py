45af3b6733daff3400c761bfb28aa8e5cff82988
Valliyappan Kannappan <105097552+ValliyappanK@users.noreply.github.com>
2022-07-17 09:52:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,7 @@ streamlit.dataframe(fruits_show)
 streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+#normalizing json text
+fruityvice_normalize=pandas.json_normalize(fruityvice_response.json())
+#print as a table
+streamlit.dataframe(fruityvice_normalize)
